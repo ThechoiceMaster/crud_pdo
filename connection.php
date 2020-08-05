57e23b4b12e43bd40dbbcff5bc406a964a89171e
ThechoiceMaster <68073926+ThechoiceMaster@users.noreply.github.com>
2020-08-05 19:05:31 +0700

    php pdo by ton

--- connection.php
+++ connection.php
@@ -6,9 +6,10 @@
 
     try {
         
-        $db = new PDO("mysql:host={$db_host}; dbname={$db_name}",$db_user, $db_password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
-    } catch (PDOEXCERPTION $e) {
+        $db = new PDO("mysql:host={$db_host}; dbname={$db_name}", $db_user, $db_password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOEXCEPTION $e) {
         $e->getMessage();
     }
 ?>
+
